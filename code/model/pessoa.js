6c8c7e4eaf1a46d11b9e2edc0e6deca5ba3d84c8
Luiz Goncalves Silva <lgoncals@everis.com>
2018-06-28 12:18:39 -0300

    Encriptação da Senha

--- code/model/pessoa.js
+++ code/model/pessoa.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database/connect');
+const bcrypt = require('bcryptjs');
 
 const PessoaSchema = new mongoose.Schema({
     name: {type: String,required: true,},
@@ -8,6 +9,12 @@ const PessoaSchema = new mongoose.Schema({
     nameMae: {type: String,required: true,},
     createdAt: {type: Date,default: Date.now,},
 });
+
+PessoaSchema.pre('save', async function(next){
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+})
 
 const Pessoa = mongoose.model('Pessoa', PessoaSchema);
 
